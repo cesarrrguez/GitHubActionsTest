d4e83f65e9676cb694864f050d7d160a7f259a6e
César Rodríguez <cesar.rrguez@gmail.com>
2020-10-28 12:31:48 +0100

    Add Hello World console project

---
+++ GitHubActionsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubActionsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
